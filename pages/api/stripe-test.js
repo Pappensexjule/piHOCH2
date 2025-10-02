961b9c736475769b6f7de45ebb019cba841573fe
Pappensexjule <jupf@me.com>
2025-10-03 01:30:51 +0200

    Update stripe-test.js

--- pages/api/stripe-test.js
+++ pages/api/stripe-test.js
@@ -1,9 +1,10 @@
 import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: "2024-09-30.acacia" });
+
 export default async function handler(req, res){
   if (req.method !== "POST") return res.status(405).json({ error: "POST only" });
   try{
-    const origin = req.headers.origin || "http://localhost:3000";
+    const origin = req.headers.origin || "https://pihoch2.me";
     const session = await stripe.checkout.sessions.create({
       mode: "payment",
       line_items: [{ price_data:{ currency:"eur", product_data:{ name:"Test" }, unit_amount:3900 }, quantity:1 }],
